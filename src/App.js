254fd35e5967f6d6e61161f09a6dfddb649d84a7
Typonote <kimradium@naver.com>
2021-10-17 18:55:23 +0900

    #6 ItemsWithImage 제작

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ChooseItems from './components/ChooseItems';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import ItemsWithImage from './components/ItemsWithImage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Header />
     <Hero/>
     <ChooseItems />
+    <ItemsWithImage />
     </>
   );
 }
